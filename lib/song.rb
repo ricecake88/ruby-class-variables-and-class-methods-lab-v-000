866dffce6c4221d516a0d45dc366c65c7166fe22
Grace Shih <vocalise@gmail.com>
2018-05-13 05:58:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,13 @@ class Song
   attr_accessor :genre, :artist
   attr_reader :name
   @@count = 0
+  @@GENRES = []
   
   def initialize(artist, name, genre)
     @@count += 1
     @name = name
+    if !@@GENRES.include?(genre)
+      @@GENRES << genre
   end
   
   def self.artists
