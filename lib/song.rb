2f14ce78e116a55f7fbd32b5e642e1d3aa510af6
Grace Shih <vocalise@gmail.com>
2018-05-13 06:46:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,17 +23,9 @@ class Song
   end
   
   def self.genre_count
-    @songs_each_genre_hash.each do |key, data|
-      @@genres.each do |genre|
-        if key == genre
-          @songs_each_genre_hash[key] += 1
-        end
-      end
-      if empty
-        @songs_each_genre[key] = 1
-      end    
+    genres.each do |genre|
+      @songs_each_genre_hash[genre] += 1
     end
-
   end
   
   def self.count
